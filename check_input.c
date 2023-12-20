fbf45b5eb32a40798065195c083f2d1c0cbdf273
Nekane <125030234+nmontiel42@users.noreply.github.com>
2023-12-20 15:40:23 +0100

    Update check_input.c

--- check_input.c
+++ check_input.c
@@ -37,38 +37,31 @@ long long	ft_atoi(const char *str)
 	return (res * sign);
 }
 
-int	ft_isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
-		return (c);
-	return (0);
-}
-
 int	is_number(char *str)
 {
 	while (*str)
 	{
-		if (ft_isdigit(*str))
-			return (0);
+		if (*str < '0' || *str > '9')
+			return (1);
 		str++;
 	}
-	return (1);
+	return (0);
 }
 
 int	validate_input(int argc, char **argv)
 {
 	if (argc != 5 && argc != 6)
 		return (printf("Error: Invalid number of arguments\n"), 0);
-	if (ft_atoi(argv[1]) < 1 || ft_atoi(argv[1]) > MAX_PHILOS)
+	if (ft_atoi(argv[1]) < 1 && ft_atoi(argv[1]) > MAX_PHILOS)
 		return (printf("Error: Num of philos must be between 1 and %d\n",
 				MAX_PHILOS), 0);
-	if (is_number(argv[2]) || ft_atoi(argv[2]) < 60)
+	if (is_number(argv[2]) && ft_atoi(argv[2]) < 60)
 		return (printf("Error: Invalid death time\n"), 0);
-	if (is_number(argv[3]) || ft_atoi(argv[3]) < 60)
+	if (is_number(argv[3]) && ft_atoi(argv[3]) < 60)
 		return (printf("Error: Invalid eating time\n"), 0);
-	if (is_number(argv[4]) || ft_atoi(argv[4]) < 60)
+	if (is_number(argv[4]) && ft_atoi(argv[4]) < 60)
 		return (printf("Error: Invalid sleeping time\n"), 0);
-	if (argc == 6 && (is_number(argv[5]) || ft_atoi(argv[5]) <= 0))
+	if (argc == 6 && (is_number(argv[5]) && ft_atoi(argv[5]) <= 0))
 		return (printf("Error: Invalid number of meals\n"), 0);
 	return (1);
 }
